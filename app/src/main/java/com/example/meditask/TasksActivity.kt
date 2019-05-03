945998f6c24ab8d628a846de6221f3a9bbb3cb46
andreip <andrei.pancu@consultant.lego.com>
2019-05-03 10:34:18 +0300

    3 - Added task layout

---
+++ app/src/main/java/com/example/meditask/TasksActivity.kt
@@ -0,0 +1,12 @@
+package com.example.meditask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
